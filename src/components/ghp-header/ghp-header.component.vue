0499e0a9b7e097b80c7a7d2dded5db8db5c3e21e
flashback2k14 <feedbackcalcest@gmail.com>
2017-06-09 14:10:32 +0200

    #5 - integrate search into header component

--- src/components/ghp-header/ghp-header.component.vue
+++ src/components/ghp-header/ghp-header.component.vue
@@ -1,8 +1,13 @@
 <template>
   <header>
-    <div class="header-title">{{ title }}</div>
-    <div class="header-logout">
-      <button class="btn-logout" @click="logout">Logout</button>
+    <div class="header-row_first">
+      <div class="header-title">{{ title }}</div>
+      <div class="header-logout">
+        <button class="btn-logout" @click="logout">Logout</button>
+      </div>
+    </div>
+    <div class="header-row_second">
+      <slot></slot>
     </div>
   </header>
 </template>
@@ -10,12 +15,31 @@
 <script src="./ghp-header.component.js"></script>
 
 <style scoped>
+  header {
+    display: flex;
+    flex-direction: column;
+    flex: 0 0 108px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: calc(100% - 24px);
+    padding: 12px;
+    background: #283593;
+    color: white;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+  }
+
+  .header-row_first {
+    display: flex;
+    flex-direction: row;
+  }
+
   .header-title {
     flex: 1 0 auto;
-    font-size: large;
+    font-size: xx-large;
   }
   .header-logout {
-    flex: 0 0 50px;
+    flex: 0 0 60px;
   }
   .btn-logout {
     width: 80px;
@@ -24,10 +48,12 @@
     border: 0;
     cursor: pointer;
     text-transform: uppercase;
-    font-size: small;
+    font-size: medium;
   }
-  .btn-logout:hover {
-    border: 1px solid #1A237E;
-    border-radius: 4px;
+
+  .header-row_second {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
