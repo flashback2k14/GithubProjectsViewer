efa9ec9786627955ad015178b0f5a14b5d5cd438
flashback2k14 <feedbackcalcest@gmail.com>
2017-06-09 16:32:14 +0200

    #5 - change buttons to icon buttons

--- src/components/ghp-header/ghp-header.component.vue
+++ src/components/ghp-header/ghp-header.component.vue
@@ -3,9 +3,15 @@
     <div class="header-row_first">
       <div class="header-title">{{ title }}</div>
       <div class="header-logout">
-        <button class="btn-logout" @click="logout">Logout</button>
+        <button class="btn-logout" @click="logout">
+          <span>logout</span>
+          <svg style="width:36px; height:36px" viewBox="0 0 36 36">
+            <path fill="#fff" d="M17,17.25V14H10V10H17V6.75L22.25,12L17,17.25M13,2A2,2 0 0,1 15,4V8H13V4H4V20H13V16H15V20A2,2 0 0,1 13,22H4A2,2 0 0,1 2,20V4A2,2 0 0,1 4,2H13Z" />
+          </svg>
+        </button>
       </div>
     </div>
+    <div class="header-spacer"></div>
     <div class="header-row_second">
       <slot></slot>
     </div>
@@ -33,16 +39,16 @@
     display: flex;
     flex-direction: row;
   }
-
   .header-title {
     flex: 1 0 auto;
     font-size: xx-large;
   }
   .header-logout {
-    flex: 0 0 60px;
+    flex: 0 0 120px;
   }
   .btn-logout {
-    width: 80px;
+    display: flex;
+    width: 120px;
     background: #283593;
     color: white;
     border: 0;
@@ -50,10 +56,32 @@
     text-transform: uppercase;
     font-size: medium;
   }
+  .btn-logout span {
+    padding-top: 3px;
+    padding-right: 8px;
+  }
+
+  .header-spacer {
+    height: 4px;
+  }
 
   .header-row_second {
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  /* Smartphones (portrait and landscape) ----------- */
+  @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+    .header-title {
+      font-size: x-large;
+    }
+    .header-logout {
+      flex: 0 0 100px;
+    }
+    .btn-logout {
+      width: 100px;
+      font-size: small;
+    }
   }
 </style>
--- src/components/ghp-search/ghp-search.component.vue
+++ src/components/ghp-search/ghp-search.component.vue
@@ -6,14 +6,26 @@
     <input type="text" class="input-style" 
             v-model="search.repo" 
             placeholder="Enter a Github Repo...">
-    <button type="submit" 
+    <button type="submit"
       class="button-style button-style_search">
-        Search
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" 
+            height="24" fill="#ffffff" class="svg-style">
+          <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 
+          13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 
+          4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 
+          9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
+          </path>
+        </svg>
     </button>
     <button type="button" 
       @click.prevent="onClear" 
       class="button-style button-style_clear">
-        Clear
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" 
+            height="24" fill="#ffffff" class="svg-style">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 
+          6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+          </path>
+        </svg>
     </button>
   </form>
 </template>
@@ -25,38 +37,62 @@
     width: 310px;
     margin-right: 20px;
     margin-bottom: 5px;
+    padding-bottom: 4px;
+    background: #283593;
+    color: #fff;
     border: 0;
-    border-bottom: 1px solid #283593;
+    border-bottom: 1px solid #1A237E;
     font-size: medium;
   }
   .input-style:hover {
     border-bottom: 1px solid #FF1744;
   }
+  .input-style::placeholder {
+    color: #BDBDBD;
+  }
 
   .button-style {
-    width: 150px;
-    padding: 3px;
-    margin-top: 5px;
+    width: 48px;
+    height: 48px;
+    margin-left: 12px;
     border: 0;
-    border-radius: 4px;
-    font-size: medium;
-    text-transform: uppercase;
+    border-radius: 50%;
     cursor: pointer;
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.4);
     transition: 0.3s;
   }
   .button-style:hover {
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.6);
   }
-
   .button-style_search {
+    background: #3D5AFE;
+    color: white;
+  }
+  .button-style_clear {
     background: #FF1744;
     color: white;
   }
 
-  .button-style_clear {
-    margin-left: 12px;
-    background: #283593;
-    color: white;
+  .svg-style {
+    padding-top: 4px;
+  }
+
+  /* Smartphones (portrait and landscape) ----------- */
+  @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+    form {
+      margin-top: 8px;
+    }
+    .input-style {
+      width: 240px;
+      margin-right: 4px;
+    }
+    .button-style {
+      width: 36px;
+      height: 36px;
+      margin-left: 4px;
+    }
+    .svg-style {
+      padding-top: 2px;
+    }
   }
 </style>
