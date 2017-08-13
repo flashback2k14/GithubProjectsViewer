5c537712ef54cf60be7b3888d96093d02829cf8b
flashback2k14 <feedbackcalcest@gmail.com>
2017-08-13 16:25:07 +0200

    #17 - implement electron app logic

--- desktop/index.js
+++ desktop/index.js
@@ -1,12 +1,19 @@
-import { app, BrowserWindow } from 'electron';
+import { app, dialog, BrowserWindow, Tray, Menu } from 'electron';
+import path from 'path';
 
-let mainWindow;
+// init vars
+let mainWindow = null;
+let willQuit = false;
+let tray = null;
 
+// init mainWindow
 const createWindow = async () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 720,
+    darkTheme: true,
+    icon: path.join(__dirname, "assets/icons/png/128x128.png")
   });
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
@@ -15,14 +22,73 @@ const createWindow = async () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    if (willQuit) {
+      mainWindow = null;
+    } else {
+      e.preventDefault();
+      mainWindow.hide();
+    }
   });
 };
+
+// init tray
+const traySetup = async () => {
+  // init tray
+  switch (process.platform) {
+    case "darwin":
+      tray = new Tray(path.join(__dirname, 'assets/icons/png/16x16.png'));
+      break;
+    case "win32":
+      tray = new Tray(path.join(__dirname, 'assets/icons/png/32x32.png'));
+      break;
+    default:
+      tray = new Tray(path.join(__dirname, 'assets/icons/png/16x16.png'));
+  }
+  // init context menu
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open GithubProjectsViewer',
+      click: () => {
+        mainWindow.show();
+      }
+    }, {
+      label: 'About',
+      click: () => {
+        dialog.showMessageBox(mainWindow,
+          {
+            type: 'info',
+            title: 'About',
+            message: 'GithubProjectsViewer is built by flashback2k14',
+            detail: 'You can find me on GitHub.',
+            icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+          }
+        );
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Quit GithubProjectsViewer',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+  // set context menu to tray
+  tray.setToolTip('GithubProjectsViewer');
+  tray.setContextMenu(contextMenu);
+  // init click listener
+  tray.on('click', () => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  });
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', async () => {
+  createWindow();
+  traySetup();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -39,4 +105,7 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
+  mainWindow.show();
 });
+
+app.on('before-quit', () => willQuit = true);
